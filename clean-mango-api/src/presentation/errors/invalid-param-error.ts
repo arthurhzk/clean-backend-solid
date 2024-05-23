ca7aabc9dd73cf32cdcf36cfab2f13b40324ba79
Arthur kunz <arthurhkunz@gmail.com>
2024-05-23 19:58:23 -0300

    test: ensure sign up controller calls validator with correct email

--- clean-mango-api/src/presentation/errors/invalid-param-error.ts
+++ clean-mango-api/src/presentation/errors/invalid-param-error.ts
@@ -1,6 +1,6 @@
 export class InvalidParamError extends Error {
   constructor(paramName: string) {
-    super(`Invalic param: ${paramName}`)
+    super(`Invalid param: ${paramName}`)
     this.name = 'InvalidParamError'
   }
 }
