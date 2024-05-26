7f1601b2cadbdb7ca7990d2421ab6ad5954d2d0d
Arthur Henrique Zarpelon Kunz <arthurhkunz@MacBook-Air-de-Arthur.local>
2024-05-26 20:03:14 -0300

    refactor: move mapping to MongoHelper

--- clean-mango-api/src/infra/db/mongodb/helpers/mongo-helper.ts
+++ clean-mango-api/src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
@@ -11,5 +12,9 @@ export const MongoHelper = {
   },
   getCollection(name: string): Collection {
     return this.client.db().collection(name)
+  },
+  map(collection: any): any {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id })
   },
 }
