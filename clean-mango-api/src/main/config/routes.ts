a3aa357423773215af27bc68c08202d056516e4b
Arthur Henrique Zarpelon Kunz <arthurhkunz@MacBook-Air-de-Arthur.local>
2024-06-01 15:06:34 -0300

    feat: add adapter design pattern to signUpController

--- clean-mango-api/src/main/config/routes.ts
+++ clean-mango-api/src/main/config/routes.ts
@@ -1,11 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+import signupRoutes from '../../../src/main/routes/signup-routes'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async (file) =>
-    (await import(`../../../${file}`)).default(router)
-  )
+
+  signupRoutes(router)
 }
